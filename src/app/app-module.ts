c36827d29b50428113debce264e00b9650d1ab6d
BoBenc <bogerbence2002@gmail.com>
2026-06-21 20:37:09 +0200

    Add Home, Navbar, and Footer components

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './navbar/navbar';
+import { Home } from './home/home';
+import { Footer } from './footer/footer';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Navbar, Home, Footer],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
